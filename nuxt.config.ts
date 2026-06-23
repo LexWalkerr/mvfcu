0ee5d294a74d68487554c15390b2d79f86747171
Good Boy <goodboy@Goodboy-MacBook-Pro.local>
2026-06-23 09:23:09 -0400

    cdcd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+    app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbNKhfbF7K8h2wi1abQn3kG5hmdBtXKTdsVg&s' }
+      ]
+    }
+  }
 })
